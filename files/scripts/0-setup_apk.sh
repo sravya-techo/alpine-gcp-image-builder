d5d1aad963a837faa2d0a10c45d270c98ca7d980
sravya-techo <98745880+sravya-techo@users.noreply.github.com>
2022-07-27 18:24:57 +0530

    Update 0-setup_apk.sh

--- files/scripts/0-setup_apk.sh
+++ files/scripts/0-setup_apk.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-echo http://dl-cdn.alpinelinux.org/alpine/v3.12/community >> /etc/apk/repositories
+echo https://dl-cdn.alpinelinux.org/alpine/v3.16/community >> /etc/apk/repositories
 apk add sudo --no-cache
